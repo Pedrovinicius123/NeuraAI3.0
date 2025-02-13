ebbfe4732ebc5350e4fddb4b9ad02834c51f7782
Pedro Vinicius de Almeida Silva Xavier <20231041110006@ifrn.local>
2025-02-13 15:47:09 -0300

    Encoding

--- core/test.py
+++ core/test.py
@@ -1,4 +1,4 @@
-import nltk, os, pickle
+import nltk, os, pickle, binascii, codecs
 import numpy as np
 
 from nltk.tokenize import word_tokenize
@@ -24,27 +24,31 @@ def vectorize_samples(samples:list):
     vectorized_samples = []    
     
     for sample in samples:
-        print(str(sample.encode()).split(' '))
-        raw=b''.join(['\x00'+e if len(e)==1 else e for e in sample.encode.split(' ')])
-        print(raw)
-        vectorized_samples.append(np.fromstring(raw))
-        
+        raw = binascii.hexlify(sample.encode())
+        vectorized_samples.append(np.frombuffer(raw, np.uint8).reshape(-1,1))
 
     return vectorized_samples
 
-def train_data_(n_strings:int, input_neuratrons:int=10, inner_neuratron_shape:int=(300, 300), lr:float=0.001):
-    samples = generate_random_strings(n_strings=n_strings)
-    vectorized_samples = vectorize_samples(samples)
-    
-    neura = Brain(input_neuratrons=input_neuratrons, inner_neuratron_shape=inner_neuratron_shape, lr=lr)
-    for x, y in zip(vectorized_samples, vectorized_samples):
-        fit(neura, 'neura1.1.pkl', x, y, 30, inner_neuratron_shape=(300, 300), lr=0.001, epochs=1500)
+def train_data_(model_name:str, n_strings:int, input_neuratrons:int=10, inner_neuratron_shape:int=(300, 300), lr:float=0.001, x:np.ndarray=None, y:np.ndarray=None):
+    if x == None:    
+        samples = generate_random_strings(n_strings=n_strings)
+        x = vectorize_samples(samples)
 
-def recusive_train_data(epochs:int=1000):
+    neura = Brain(input_neuratrons=input_neuratrons, inner_neuratron_shape=inner_neuratron_shape, lr=lr)
+
+    if y == None:      
+        for x_inner, y_inner in zip(x, x):
+            fit(neura, model_name, x_inner, y_inner, 30, inner_neuratron_shape=(300, 300), lr=0.001, epochs=1500)
+
+    else:
+        for x_inner, y_inner in zip(x, y):
+            fit(neura, model_name, x_inner, y_inner, 30, inner_neuratron_shape=(300, 300), lr=0.001, epochs=1500)
+            
+def recusive_train_data(epochs, filename:str):
     results = []
     model = None
 
-    with open(os.path.join('models', 'neura1.1.pkl'), 'rb') as file:
+    with open(os.path.join('models', filename), 'rb') as file:
         model = pickle.load(file)
         samples = generate_random_strings(n_strings=30)
         samples = vectorize_samples(samples)
@@ -59,17 +63,28 @@ def recusive_train_data(epochs:int=1000):
             new_samples.append(tot)
 
         results = [model.forward(sample, sample.shape[1]) for sample in new_samples]
-        print(results.tobytes().decode())
 
         for result, sample in zip(results, new_samples):
             model.fit(result[0].detach().numpy(), sample, epochs=epochs)
 
 
-    with open(os.path.join('models', 'neura1.1.pkl'), 'wb') as file:
-        pickle.dump(model)
+    with open(os.path.join('models', filename), 'wb') as file:
+        pickle.dump(model, file)
 
-    recusive_train_data(int=1000)   
+def test_model(filename:str):
+    with open(os.path.join('models', filename), 'rb') as file:
+        model = pickle.load(file)
+        sample_strings = generate_random_strings(n_strings=30)
+        vectorized = vectorize_samples(sample_strings)
+
+        results = []
+        vhex = np.vectorize(hex)
+        
+        for string in vectorized:
+            results.append(np.round(model.forward(string, string.shape[1])[0].detach().numpy()).astype(int))
+
 
 if __name__ == '__main__':
-    train_data_(1)
-    recusive_train_data()
+    train_data_('lingua1.0.pkl', 10)
+    recusive_train_data(1000, 'lingua1.0.pkl')
+    test_model('lingua1.0.pkl')
